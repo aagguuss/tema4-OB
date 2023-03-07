5359218f4686a797d07c66273f9ce9b1ad655a2e
Agustin Emilio <agustinesimiand@gmail.com>
2023-03-07 12:13:48 -0300

    get commit

---
+++ WhileOb/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+      int contador = 1;
+
+      while (contador < 3)  {
+          System.out.println(contador);
+                contador=contador+1;
+                }
+        System.out.println(contador);
+    }
+}
